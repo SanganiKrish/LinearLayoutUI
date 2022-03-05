05beefb386d6d1d652a7c321cedb45af9028d335
SanganiKrish <krish.noor2021@gmail.com>
2022-03-05 11:20:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/linearlayoutui/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.linearlayoutui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
